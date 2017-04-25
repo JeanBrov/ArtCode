7c1ba19bc761efbd95857f5a3c95f93e7c5e61bb
Ivan.Kriskun <Ivan.Kriskun@gmail.com>
2017-04-25 16:54:09 +0300

    Second test

--- .idea/src/week1/Task4_1.java
+++ .idea/src/week1/Task4_1.java
@@ -1,23 +1,8 @@
-import java.util.Scanner;
-
 /**
- * 4.1. Пользователь вводит параметр с консоли (текущий час от 0 до 24)
- Если время от 9 до 18, то выводим "Я на работе", в другом случае "Я отдыхаю")
+ * Created by Ivan.Kriskun on 25.04.2017.
  */
 public class Task4_1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String timeString = scanner.toString();
-        System.out.println("timeString= "+timeString);
-        int timeDigit=-1;
-//        try {
-            timeDigit = Integer.parseInt(timeString);
-//        } catch (Exception e){
-//            System.out.println("Please type digit from 0 to 24");
-//        }
-        if ((timeDigit>=9)&&(timeDigit<=18)) {System.out.printf("Я на работе");}
-        else if (((timeDigit>=0)&&(timeDigit<9))||((timeDigit>18)&&(timeDigit<24))){System.out.println("Я отдыхаю");}
-        else System.out.println("Incorrect digit");
-        }
-
+        System.out.println("OPPPPPPA");
     }
+}
