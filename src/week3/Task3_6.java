762e1e24529f609f84b19d231fb52c00107f6569
Ivan.Kriskun <Ivan.Kriskun@gmail.com>
2017-05-05 21:07:00 +0300

    Base course. Week2.Task 3.6

--- src/week3/Task3_6.java
+++ src/week3/Task3_6.java
@@ -23,32 +23,59 @@ public class Task3_6 {
 
     }
 
-    public static void onesZerosFinding(String str){
+    private static void onesZerosFinding(String str){
         int length=str.length();
         int[] arr=new int[length];
         for (int i = 0; i <length ; i++) {
             arr[i]=Character.getNumericValue(str.charAt(i));
-//            System.out.println(arr[i]);
         }
-        int startPositionOne;
-        int finishPositionOne;
-        int startPositionZero;
-        int finishPositionZero;
 
+        int finishPositionOne=-1;
+        int finishPositionZero=-1;
         int counterZero=0;
         int counterOne=0;
-        boolean tagOne=false;
-        boolean tagZero=false;
-        
-
-
+        int max_Zeros=0;
+        int max_Ones=0;
         for (int i = 0; i <arr.length ; i++) {
-            if (arr[i]==0){tagZero=true;}
-            if (arr[i]==1){tagOne=true;}
-            if (arr[i]==0){
-                counterOne++;
 
+//            Logic of counting Zeroes(Ones). We calculate the maximum of zeroes(ones) and find position of the last digit than
+//            finding position of start chain.
+
+            if ((arr[i]==0)){
+                counterZero++;
+            }else  {
+                if (max_Zeros<counterZero){
+                    max_Zeros=counterZero;
+                    finishPositionZero=i;
+                }
+                counterZero=0;
+            }
+            if ((arr[i]==0)&&(i==(arr.length-1))){
+                if (max_Zeros<counterZero){
+                    max_Zeros=counterZero;
+                    finishPositionZero=i;
+                }
+            }
+//
+            if ((arr[i]==1)){
+                counterOne++;
+            }else  {
+                if (max_Ones<counterOne){
+                    max_Ones=counterOne;
+                    finishPositionOne=i;
+                }
+                counterOne=0;
+            }
+            if ((arr[i]==0)&&(i==(arr.length-1))){
+                if (max_Ones<counterOne){
+                    max_Ones=counterOne;
+                    finishPositionOne=i;
+                }
             }
         }
+        int startPositionZero=finishPositionZero-max_Zeros+1;
+        int startPositionOne=finishPositionOne-max_Ones+1;
+        System.out.println("("+startPositionZero+","+ finishPositionZero+") - цепочка нулей");
+        System.out.println("("+startPositionOne+","+finishPositionOne+") - цепочка едениц");
     }
 }
